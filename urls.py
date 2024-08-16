c3f7bb3e48c7047b52adb893a4690c2c13d369a7
DmitryFolomin <161714326+DmitryFolomin@users.noreply.github.com>
2024-08-16 11:41:54 +0300

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,7 +1,24 @@
-from django.urls import path
-from . import views
+"""
+URL configuration for my_page project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/dev/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+
 
 urlpatterns = [
-    path('<int:sign_zodiac>', views.get_info_about_sign_zodiac_by_number),
-    path('<str:sign_zodiac>', views.get_info_about_sign_zodiac, name='horoscope-name'),
+    path('admin/', admin.site.urls),
+    path('horoscope/', include('horoscope.urls')),
 ]
